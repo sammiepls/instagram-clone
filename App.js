76e7be4e82a86ca53ecf7a2ff63cabfd0a0ff270
Samantha Lau <samantha.lhe@gmail.com>
2021-02-06 15:10:54 +0800

    Add firebase config and initialization

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Config from "react-native-config";
+import React from "react";
+import * as firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import Landing from "./screens/auth/Landing";
+import Register from "./screens/auth/Register";
+
+const firebaseConfig = {
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
