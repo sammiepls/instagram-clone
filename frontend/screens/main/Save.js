268dd4eea847dfff25903b1f718e8a24078a7c21
Samantha Lau <samantha.lhe@gmail.com>
2021-02-10 20:20:13 +0800

    Add firestore function

--- frontend/screens/main/Save.js
+++ frontend/screens/main/Save.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import firebase from "firebase";
-import "firebase/firestore";
-import "firebase/firebase-storage";
-import { StyleSheet, Text, Image, TouchableOpacity, TextInput, View } from "react-native";
+import React, { useState } from 'react';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/firebase-storage';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function Save({ route, navigation }) {
   const { image } = route.params;
-  const [caption, setCaption] = useState("");
+  const [caption, setCaption] = useState('');
 
   const uploadImage = async () => {
     const response = await fetch(image);
@@ -14,27 +21,34 @@ export default function Save({ route, navigation }) {
     const task = firebase
       .storage()
       .ref()
-      .child(`post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`)
+      .child(
+        `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`,
+      )
       .put(blob);
 
-    const taskProgress = (snapshot) => console.log("transferred: " + snapshot.bytesTransferred);
+    const taskProgress = (snapshot) =>
+      console.log('transferred: ' + snapshot.bytesTransferred);
 
-    const taskCompleted = () => task.snapshot.ref.getDownloadURL().then((snapshot) => savePostData(snapshot));
+    const taskCompleted = () =>
+      task.snapshot.ref
+        .getDownloadURL()
+        .then((snapshot) => savePostData(snapshot));
 
     const taskError = (snapshot) => console.log(snapshot);
 
-    task.on("state_changed", taskProgress, taskError, taskCompleted);
+    task.on('state_changed', taskProgress, taskError, taskCompleted);
   };
 
   const savePostData = (downloadURL) => {
     firebase
       .firestore()
-      .collection("posts")
+      .collection('posts')
       .doc(firebase.auth().currentUser.uid)
-      .collection("userPosts")
+      .collection('userPosts')
       .add({
         downloadURL,
         caption,
+        likesCount: 0,
         creation: firebase.firestore.FieldValue.serverTimestamp(),
       })
       .then(() => navigation.popToTop());
@@ -45,8 +59,16 @@ export default function Save({ route, navigation }) {
       <Image style={styles.image} source={{ uri: image }} />
       <View style={styles.captionContainer}>
         <Text style={styles.header}>Write a caption:</Text>
-        <TextInput style={styles.caption} value={caption} placeholder="..." onChangeText={setCaption} />
-        <TouchableOpacity style={styles.saveButton} onPress={() => uploadImage()}>
+        <TextInput
+          style={styles.caption}
+          value={caption}
+          placeholder="..."
+          onChangeText={setCaption}
+        />
+        <TouchableOpacity
+          style={styles.saveButton}
+          onPress={() => uploadImage()}
+        >
           <Text style={styles.saveButtonText}>Save</Text>
         </TouchableOpacity>
       </View>
@@ -63,17 +85,17 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   caption: { marginTop: 10, marginBottom: 20 },
-  header: { fontWeight: "bold", letterSpacing: 3, textTransform: "uppercase" },
+  header: { fontWeight: 'bold', letterSpacing: 3, textTransform: 'uppercase' },
   saveButton: {
-    backgroundColor: "#5b6abf",
+    backgroundColor: '#5b6abf',
     padding: 10,
     borderRadius: 10,
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   saveButtonText: {
-    color: "white",
-    fontWeight: "bold",
-    textTransform: "uppercase",
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
     letterSpacing: 5,
   },
 });
