29735e39de30f86fa2794ad0ee9b90bfc92b027a
Samantha Lau <samantha.lhe@gmail.com>
2021-02-10 11:41:31 +0800

    Add functionality to FeedScreen

--- screens/main/Feed.js
+++ screens/main/Feed.js
@@ -1,12 +1,59 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  ActivityIndicator,
+  Image,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { useSelector } from 'react-redux';
 
 export default function Feed() {
+  const { users, usersLoaded } = useSelector((state) => state.usersState);
+  const { currentUser, following } = useSelector((state) => state.userState);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let p = [];
+    if (usersLoaded === following.length) {
+      for (let i = 0; i < following.length; i++) {
+        const user = users.find((u) => u.uid === following[i]);
+        if (user) {
+          p = [...p, ...user.posts];
+        }
+      }
+
+      p.sort((x, y) => x.creation - y.creation);
+      setPosts(p);
+    }
+  }, [usersLoaded, following, users]);
+
   return (
-    <View>
-      <Text>Feed</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {posts ? (
+        <FlatList
+          data={posts}
+          keyExtractor={(p) => p.id}
+          renderItem={({ item }) => (
+            <View style={styles.imageContainer}>
+              <Text>{item.user.name}</Text>
+              <Image style={styles.image} source={{ uri: item.downloadURL }} />
+            </View>
+          )}
+        />
+      ) : (
+        <ActivityIndicator />
+      )}
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  image: {
+    flex: 1,
+    aspectRatio: 1 / 1,
+  },
+});
